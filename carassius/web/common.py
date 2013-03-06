386b843105c20dcbf1d536bf6f9c0d59962a0a45
Siebe Joris Jochems <siebz0r@gmail.com>
2013-03-06 22:53:29 +0100

    User authentication works

--- carassius/web/common.py
+++ carassius/web/common.py
@@ -17,12 +17,12 @@ class RegistrationHandler(webapp2.RequestHandler):
         password = PasswordField("Password", [DataRequired(), EqualTo('password_verify', "Passwords must match.")])
         password_verify = PasswordField("Verify password", [EqualTo('password', "")])
         email_address = StringField("Email address", [DataRequired(), Email()])
-    
+
     def get(self):
         form = self.RegistrationForm()
         template_values = { "form" : form }
         self.render(template_values)
-        
+
     def post(self):
         form = self.RegistrationForm(self.request.params)
         if form.validate():
@@ -34,12 +34,34 @@ class RegistrationHandler(webapp2.RequestHandler):
         else:
             template_values = { "form" : form }
             self.render(template_values)
-            
+
     def render(self, template_values):
         template = jinja2.env.get_template("register.html")
         self.response.out.write(template.render(template_values))
 
 class LoginHandler(webapp2.RequestHandler):
+    class LoginForm(Form):
+        user_name = StringField("User name", [DataRequired()])
+        password = PasswordField("Password", [DataRequired()])
+
     def get(self):
+        form = self.LoginForm()
+        template_values = { 'form' : form }
+        self.render(template_values)
+
+    def post(self):
+        form = self.LoginForm(self.request.params)
+        if form.validate():
+            p = Person.authenticate(form.user_name.data, form.password.data)
+            if p:
+                pass
+                #TODO: redirect to home or something.
+            else:
+                form.errors['credentials'] = "User name or password incorrect."
+                self.render({ 'form' : form })
+        else:
+            self.render({ 'form' : form })
+
+    def render(self, template_values):
         template = jinja2.env.get_template("login.html")
-        self.response.out.write(template.render())
+        self.response.out.write(template.render(template_values))
