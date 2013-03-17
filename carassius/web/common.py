c45cfbdf3413351f8662b8e3581e7d0cc7a6122a
Siebe Joris Jochems <siebz0r@gmail.com>
2013-03-17 23:24:46 +0100

    Configured Jinja2 to be used as a cached property.

--- carassius/web/common.py
+++ carassius/web/common.py
@@ -3,10 +3,11 @@ Created on Feb 24, 2013
 
 @author: siebz0r
 '''
-import jinja2
+from argparse import ArgumentError
 from model.common import Person
 from model.common import RegistrationActivationToken
 import webapp2
+from webapp2_extras import jinja2
 from wtforms.fields.core import StringField
 from wtforms.fields.simple import PasswordField
 from wtforms.form import Form
@@ -15,7 +16,21 @@ from wtforms.validators import Email
 from wtforms.validators import EqualTo
 from wtforms.validators import Length
 
-class RegistrationHandler(webapp2.RequestHandler):
+class TemplateHandler(webapp2.RequestHandler):
+    @webapp2.cached_property
+    def jinja2(self):
+        return jinja2.get_jinja2()
+
+    def render_response(self, template=None, **context):
+        template = template or self.template
+        if not template:
+            raise ArgumentError(
+                        "A template must supplied as property or as argument.")
+        self.response.write(self.jinja2.render_template(template, **context))
+
+class RegistrationHandler(TemplateHandler):
+    template = "register.html"
+
     class RegistrationForm(Form):
         user_name = StringField("User name", [DataRequired(), Length(3, message="Must be at least %(min)d characters long.")])
         password = PasswordField("Password", [DataRequired(), EqualTo('password_verify', "Passwords must match.")])
@@ -24,8 +39,7 @@ class RegistrationHandler(webapp2.RequestHandler):
 
     def get(self):
         form = self.RegistrationForm()
-        template_values = { "form" : form }
-        self.render(template_values)
+        self.render_response(form=form)
 
     def post(self):
         form = self.RegistrationForm(self.request.params)
@@ -34,25 +48,21 @@ class RegistrationHandler(webapp2.RequestHandler):
             person.user_name = form.user_name.data
             person.email_address = form.email_address.data
             Person.register(person, form.password.data)
-            self.render({"success": True,
-                         "email_address": person.email_address})
+            self.render_response(success=True,
+                                 email_address=person.email_address)
         else:
-            template_values = { "form" : form }
-            self.render(template_values)
+            self.render_response(form=form)
 
-    def render(self, template_values):
-        template = jinja2.env.get_template("register.html")
-        self.response.out.write(template.render(template_values))
+class LoginHandler(TemplateHandler):
+    template = "login.html"
 
-class LoginHandler(webapp2.RequestHandler):
     class LoginForm(Form):
         user_name = StringField("User name", [DataRequired()])
         password = PasswordField("Password", [DataRequired()])
 
     def get(self):
         form = self.LoginForm()
-        template_values = { 'form' : form }
-        self.render(template_values)
+        self.render_response(form=form)
 
     def post(self):
         form = self.LoginForm(self.request.params)
@@ -63,25 +73,18 @@ class LoginHandler(webapp2.RequestHandler):
                 #TODO: redirect to home or something.
             else:
                 form.errors['credentials'] = "User name or password incorrect."
-                self.render({ 'form' : form })
+                self.render_response(form=form)
         else:
-            self.render({ 'form' : form })
-
-    def render(self, template_values):
-        template = jinja2.env.get_template("login.html")
-        self.response.out.write(template.render(template_values))
+            self.render_response(form=form)
 
 
-class ActivationHandler(webapp2.RequestHandler):
+class ActivationHandler(TemplateHandler):
+    template = "activate.html"
     def get(self, token):
         token = RegistrationActivationToken.query(
                               RegistrationActivationToken.token == token).get()
         if token:
             token.activate()
-            self.render(True)
+            self.render_response(success=True)
         else:
-            self.render(False)
-
-    def render(self, success):
-        template = jinja2.env.get_template("activate.html")
-        self.response.out.write(template.render({ 'success' : success }))
+            self.render_response(success=False)
--- carassius/web/main.py
+++ carassius/web/main.py
@@ -3,7 +3,6 @@ Created on Feb 24, 2013
 
 @author: siebz0r
 '''
-import jinja2
 import os
 import sys
 import webapp2
@@ -13,12 +12,6 @@ from web.common import LoginHandler
 
 for x in os.listdir('lib'):
     sys.path.append(os.path.abspath('lib/%s' % x))
-
-env_path = __file__
-for i in range(3):
-    env_path = os.path.dirname(env_path)
-env_path = os.path.join(env_path, "templates")
-jinja2.env = jinja2.Environment(loader=jinja2.FileSystemLoader(env_path))
 
 routes = [(r"/register/?", RegistrationHandler),
           (r"/login/?", LoginHandler),
